cda19ff2b54baab23a99ab24d5ae4003ca58b91c
Luciferka18 <ahmetzanov.samat.lite@gmail.com>
2026-02-09 00:41:57 +0300

    Add legacy non-api trainer routes to prevent Next.js 404

--- fitlab/routes/web.php
+++ fitlab/routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProgramController;
+use App\Http\Controllers\Api\TrainerController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| API compatibility routes (without /api prefix)
+|--------------------------------------------------------------------------
+|
+| Some client apps call legacy URLs like /trainers/{id} directly.
+| Keep read-only aliases to prevent 404 while primary API stays under /api/*.
+|
+*/
+Route::get('/trainers', [TrainerController::class, 'index']);
+Route::get('/trainers/{id}', [TrainerController::class, 'show']);
+
+Route::get('/programs', [ProgramController::class, 'index']);
+Route::get('/programs/{id}', [ProgramController::class, 'show']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{article}', [ArticleController::class, 'show']);
